b1a8baf79ce4b57d23b52f13b27a06b3664869ff
Manojbabu <83714923+manusoft@users.noreply.github.com>
2023-12-15 19:26:53 +0400

    Feature - update core module (#15)
    * Update core module (YoutubeExplode)
    
    * Update UI small changes
    
    ---------
    
    Co-authored-by: manuhome <manu@home>

--- VidSync/MainWindow.xaml.cs
+++ VidSync/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace VidSync;
+﻿using System;
+
+namespace VidSync;
 
 public sealed partial class MainWindow : WindowEx
 {
@@ -13,7 +15,7 @@ public sealed partial class MainWindow : WindowEx
         AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
         AppWindow.Resize(new Windows.Graphics.SizeInt32(800, 600));
         Content = null;
-        Title = "AppDisplayName".GetLocalized();
+        Title = GetVersionDescription();
 
         // Theme change code picked from https://github.com/microsoft/WinUI-Gallery/pull/1239
         dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
@@ -30,5 +32,23 @@ public sealed partial class MainWindow : WindowEx
         {
             TitleBarHelper.ApplySystemThemeToCaptionButtons();
         });
+    }
+
+    private static string GetVersionDescription()
+    {
+        Version version;
+
+        if (RuntimeHelper.IsMSIX)
+        {
+            var packageVersion = Package.Current.Id.Version;
+
+            version = new(packageVersion.Major, packageVersion.Minor, packageVersion.Build, packageVersion.Revision);
+        }
+        else
+        {
+            version = Assembly.GetExecutingAssembly().GetName().Version!;
+        }
+
+        return $"{"AppDisplayName".GetLocalized()} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
     }
 }
